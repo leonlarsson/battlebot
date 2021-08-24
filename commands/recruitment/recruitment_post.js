5139207eb9d35649bc050b1be40432332c63575a
Leon San José Larsson <leonlarsson8@gmail.com>
2021-08-24 14:22:05 +0200

    remove comment

--- commands/recruitment/recruitment_post.js
+++ commands/recruitment/recruitment_post.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "recruitment_post",
     allowed_channels: ["739938247089848351", "846797227561058305"], // #recruitment, #mozzy-spam
-    cooldown: 172800000, // Seconds
+    cooldown: 172800000,
     // cooldown_exempt: ["99182302885588992", "106391128718245888"], // Mozzy, Dragory
     public: true,
     enabled: true,
