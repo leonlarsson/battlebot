8c6517fba4842a40a36f95cde3322a9225f59218
Leon San José Larsson <leonlarsson8@gmail.com>
2021-11-05 15:56:06 +0100

    let canvas create event name background
    Uses measureText and some math

--- utils/createCountdownCanvas.js
+++ utils/createCountdownCanvas.js
@@ -10,18 +10,31 @@ module.exports = async (interaction, Event) => {
 
         const canvas = Canvas.createCanvas(1000, 400);
         const context = canvas.getContext("2d");
+        const backgroundImage = await Canvas.loadImage(`./assets/images/BG_2042_${backgroundNum}.png`);
         // const backgroundImage = await Canvas.loadImage(Event.CountdownImage());
-        const backgroundImage = await Canvas.loadImage(`./assets/images/BG_BF_${backgroundNum}.png`);
+        // const backgroundImage = await Canvas.loadImage("./assets/images/BG_BF_ONLY_LOGO.png");
 
         registerFont('./assets/fonts/Geometos.ttf', { family: "Geometos" });
 
+        // Background image. (With the game logo)
         context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 
+        // Event name background
         context.font = '35px Geometos';
+        const eventName = context.measureText(Event.EventName);
+        context.fillStyle = "rgba(0, 0, 0, .4)";
+        context.fillRect((canvas.width / 2 - eventName.width / 2) - 10, 175, eventName.width + 20, 40);
+
+        //Event name text
         context.fillStyle = '#00ffde';
         context.textAlign = "center";
         context.fillText(Event.EventName, 500, 208);
 
+        // Countdown background
+        context.fillStyle = "rgba(0, 0, 0, .6)";
+        context.fillRect(0, 250, canvas.width, 70);
+
+        // Countdown text
         context.font = '50px Geometos';
         context.fillStyle = '#00ffde';
         context.textAlign = "center";
