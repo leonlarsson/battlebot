255bc014b54f7b7ad5e51e2c8c8e4bb8bbae5f56
Leon San José Larsson <leonlarsson8@gmail.com>
2021-11-13 02:30:43 +0100

    lower portal post cooldown

--- commands/portal/portal_post.js
+++ commands/portal/portal_post.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "portal_post",
     allowed_channels: ["908101543646089236"], // #portal-sharing, #mozzy-spam
     wrong_channel_message: "This is only available in <#908101543646089236>",
-    cooldown: 86400000,
+    cooldown: 43200000,
     public: true,
     enabled: true,
     async execute(interaction) {
