e6b86e520a03aa3803401d6441982b92316edf3d
Leon San José Larsson <leonlarsson8@gmail.com>
2022-03-18 01:41:23 +0100

    createFNBEvent: add EU/NA start times

--- utils/createFNBEvent.js
+++ utils/createFNBEvent.js
@@ -1,4 +1,10 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";
+import advancedFormat from "dayjs/plugin/advancedFormat.js";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(advancedFormat);
 import { CronJob } from "cron";
 import { environment } from "../config.js";
 
@@ -19,6 +25,7 @@ export default client => {
 
             const guild = client.guilds.cache.get(guildId);
             const fnbStart = dayjs().day(5).hour(21).minute(30);
+            const fnbNAStart = dayjs().day(6).hour(2).minute(0);
             const fnbEnd = dayjs().day(6).hour(5).minute(0);
 
             // Return if guild is not there
@@ -27,7 +34,7 @@ export default client => {
             // Build and create event
             guild.scheduledEvents.create({
                 name: `#FridayNightBattlefield - ${fnbStart.format("MMMM D")}`,
-                description: "Welcome to **#FridayNightBattlefield**, a weekly event where players get together to play Battlefield in a friendly atmosphere with DICE developers and Electronic Arts staff. It is a long-standing event with deep roots in the Battlefield community.\n\nThe event is hosted in multiple languages, has many dedicated servers for everyone to join in on.\nFor more information look in <#907954362637234246>.",
+                description: `Welcome to **#FridayNightBattlefield**, a weekly event where players get together to play Battlefield in a friendly atmosphere with DICE developers and Electronic Arts staff. It is a long-standing event with deep roots in the Battlefield community.\n\nThe event is hosted in multiple languages, has many dedicated servers for everyone to join in on.\nFor more information look in <#907954362637234246>.\n\n__**Start times**__\n🇪🇺 EU: <t:${fnbStart.unix()}:R> (${fnbStart.utc().format("MMM D, hh:mm A")} UTC)\n🇺🇸 NA: <t:${fnbNAStart.unix()}:R> (${fnbNAStart.tz("America/New_York").format("MMM D, hh:mm A z")})`,
                 privacyLevel: "GUILD_ONLY",
                 entityType: "EXTERNAL",
                 entityMetadata: { location: "The FridayNightBattlefield Category" },
