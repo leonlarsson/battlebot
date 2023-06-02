261564ce5bad9cbdaaef6b904f9524e250568483
Leon San José Larsson <leonlarsson8@gmail.com>
2023-06-02 16:20:58 +0200

    update auto-publishing ID again

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,7 +2,7 @@ export const name = "messageCreate";
 export function execute(message) {
 
 	const directCommChannel = "908502197170503740";
-	const webhookId = "1110614643145183234";
+	const webhookId = "1113160068171575297";
 
 	if (message.channelId === directCommChannel && message.webhookId === webhookId) {
 		console.log(`Auto-publishing new post in ${message.channel.name}`);
